83206a4f4048f0e2b299e212fac51618ab08ad3b
Antonio <jss9960@hotmail.com>
2017-08-21 13:32:33 -0700

    agregando editor de parametros a interfaz

---
+++ defaultParams.js
@@ -0,0 +1,11 @@
+//vars
+dolphinNumbers = 3;
+//fitnessFunction = x(:,1).^2+x(:,2).^2;
+dimensionalSpaceMax = [5,5];
+dimensionalSpaceMin = [-5,-5];
+iteration = 20;
+mDirection = 3;
+speed = 1;
+time = 3;
+maxFitnessValue =0;
+maximumSearchTime = 1;
---
+++ render.js
@@ -0,0 +1,82 @@
+
+	var canvasx = document.getElementById('canvasx');
+	canvasx.width = 400;
+	canvasx.height = 20;
+	var canvasy = document.getElementById('canvasy');
+	canvasy.width = 20;
+	canvasy.height = 400;
+	var canvas = document.getElementById('canvas');
+	canvas.width = 400;
+	canvas.height = 400;
+	var ctxx = canvasx.getContext('2d');
+	var ctxy = canvasy.getContext('2d');
+	var ctx = canvas.getContext('2d');
+	ctx.ruler = function (dimMax, dimMin, pos) {
+		diff = dimMax[0]-dimMin[0];
+		div = diff/10;
+		if(pos=="x"){
+			actual=dimMin[0];
+			for(i=0; i<10; i++){
+				ctxx.font = "15px Arial";
+				ctxx.fillText(actual,40*i,15);
+				ctxx.moveTo(40*i,0);
+				ctxx.lineTo(40*i,40);
+				ctxx.stroke();
+				actual = actual+div;
+			}
+		}else{
+			actual=dimMin[0];
+			for(i=0; i<10; i++){
+				ctxy.font = "15px Arial";
+				ctxy.fillText(actual*-1,5,40*i);
+				ctxy.moveTo(0,40*i);
+				ctxy.lineTo(40,40*i);
+				ctxy.stroke();
+				actual = actual+div;
+			}
+		}
+		
+	};
+	ctx.ruler(dimensionalSpaceMax, dimensionalSpaceMin, "x");
+	ctx.ruler(dimensionalSpaceMax, dimensionalSpaceMin, "y");
+	ctx.setDolphins = function(dolphins){
+		diff = dimensionalSpaceMax[0]-dimensionalSpaceMin[0];
+		div = (diff/10)*40;
+		
+		for(i=0; i<dolphins.length; i++){
+			ctx.beginPath();
+			console.log(dolphins[i].individual);
+			document.getElementById("dolphinsText").value=document.getElementById("dolphinsText").value+", "+dolphins[i].individual;
+			ctx.arc(((dolphins[i].dolp[0])*div)+200,(((dolphins[i].dolp[1]*div))*-1)+200,5,0,2*Math.PI);
+			ctx.stroke();
+			
+		}
+		
+	};
+	
+	ctx.setK = function(dolK){
+		diff = dimensionalSpaceMax[0]-dimensionalSpaceMin[0];
+		div = (diff/10)*40;
+		
+		for(i=0; i<dolK.length; i++){
+			for(j=0; j<dolK[i].M.length; j++){
+				for(k=0; k<dolK[i].M[j].length; k++){
+					console.log(dolK[i].M[j][k]);
+					ctx.beginPath();
+					console.log(dolphins[i].individual);
+					document.getElementById("dolphinsText").value=document.getElementById("dolphinsText").value+", "+dolphins[i].individual;
+					ctx.arc(((dolK[i].M[j][k][0])*div)+200,(((dolK[i].M[j][k][1]*div))*-1)+200,2,0,2*Math.PI);
+					ctx.stroke();
+				}
+			}
+			
+			
+		}
+		
+	};
+	
+	ctx.Clear = function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	};
+	ctx.Clear();
+
