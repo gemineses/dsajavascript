bb772f83b80043253840d8bce6e2cbc67b35ef9d
Antonio <jss9960@hotmail.com>
2017-11-28 18:43:09 -0800

    agregando difuso

--- js/dsaFuzzy.js
+++ js/dsaFuzzy.js
@@ -1,47 +1,55 @@
-	var obj = {
-		crisp_input: [150, 10, 10],
-		variables_input: [
-			{
-				name: "Iteraciones",
-				setsName: ["Baja", "Media", "Alta"],
+	dsaFuzzyIte = 400;
+	//tiene que sumar el 100%
+	
+	itePorBaja = .10;
+	iteCalBaja = dsaFuzzyIte * itePorBaja+itePorBaja;
+	console.log(iteCalBaja);
+	itePorMedia = .30;
+	iteCalMedia = (dsaFuzzyIte * itePorMedia) + iteCalBaja; 
+	console.log(iteCalMedia);
+	itePorAlta = .30;
+	iteCalAlta = (dsaFuzzyIte * itePorAlta) + iteCalMedia;
+	console.log(iteCalAlta);
+	
+	
+	
+	setParamsOnFuzzyByIterations = function(){
+		var fuzzyIterations = {
+			crisp_input: [10, 10, 10],
+			variables_input: [
+				{
+					name: "Iteraciones",
+					setsName: ["Baja", "Media", "Alta"],
+					sets: [
+						[0,0,iteCalBaja,iteCalMedia],
+						[iteCalBaja,iteCalMedia,iteCalMedia,iteCalAlta],
+						[iteCalMedia,iteCalAlta,dsaFuzzyIte,dsaFuzzyIte]
+					]
+				},
+			],
+			variable_output: {
+				name: "Velocidad del delfin",
+				setsName: ["Muy Lento", "Lento", "Rapido"],
 				sets: [
-					[0,0,25,150],
-					[25,150,150,300],
-					[150,300,400,400]
-				]
-			}/*,
-			{
-				name: "Ammo Status",
-				setsName: ["Low", "Okay", "Loads"],
-				sets: [
-					[0,0,0,10],
-					[0,10,10,30],
-					[10,30,40,40]
+					[0,0,0,50],
+					[0,50,50,100],
+					[50,100,100,100]
 				]
 			},
-			{
-				name: "Defense",
-				setsName: ["Light", "Medium", "Heavy"],
-				sets: [
-					[0,0,0,10],
-					[0,10,10,30],
-					[10,30,40,40]
-				]
-			}*/
-		],
-		variable_output: {
-			name: "Desirability",
-			setsName: ["Undesirable", "Desirable", "Very Desirable"],
-			sets: [
-				[0,0,25,50],
-				[25,50,50,75],
-				[50,75,100,100]
+			inferences: [
+				[2,1,0]
 			]
-		},
-		inferences: [
-			[0,2,0],
-			[0,1,2],
-			[2,1,0]
-		]
-	};
+		};
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	var fl = new FuzzyLogic();
